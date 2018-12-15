b860c81f9a7a72115529327d41cb8f2331ef11d8
Serban Patriciu <serban_patriciu@hotmail.com>
2018-12-14 21:45:23 -0500

    Implementarea de baza a limbilor ceasului.

--- blog/2018/12/12/svg_analog_clock.js
+++ blog/2018/12/12/svg_analog_clock.js
@@ -1,25 +1,57 @@
-setupLogic();
-// console.log("A01");
-function setupLogic() {
-    var xc = 150;
-    var yc = 150;
-    var raza_secundar = 130; 
+class OraCuZecimale {
+    constructor(data) {
+        if (!data instanceof Date) {
+            throw "data nu este de tip Date.";
+        }
+        this.ore = data.getHours() % 12
+                 + data.getMinutes() / 60; 
+        this.minute = data.getMinutes()
+                    + data.getSeconds() / 60; 
+        this.secunde = data.getSeconds()
+                     + data.getMilliseconds() / 1000;
+    }
+}
+
+class Punct {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var centru = new Punct(150, 150);
     var orar = document.getElementById("orar");
     var minutar = document.getElementById("minutar");
     var secundar = document.getElementById("secundar");
 
+    seteazaCeas(centru, orar, minutar, secundar);
+
     window.setInterval(() => {
-        var dataCurenta = new Date();
-        var ore = dataCurenta.getHours() % 12
-                + dataCurenta.getMinutes() / 60;
-        var minute = dataCurenta.getMinutes()
-                   + dataCurenta.getSeconds() / 60;
-        var secunde = dataCurenta.getSeconds()
-                    + dataCurenta.getMilliseconds() / 1000;
-        var x2_secunde = xc + raza_secundar * Math.sin(secunde * 2 * Math.PI / 60);
-        var y2_secunde = yc - raza_secundar * Math.cos(secunde * 2 * Math.PI / 60);
-        secundar.setAttribute("x2", x2_secunde);
-        secundar.setAttribute("y2", y2_secunde);
-        // console.log(ore, minute, secunde);
-    }, 50);
+        seteazaCeas(centru, orar, minutar, secundar);
+    }, 250);
+
+    document.getElementById("ceas").style.visibility = "visible";
+});
+
+function pozitieLimba(centru, raza, cantitate, cantitate_maxima) {
+    var punct = new Punct(0, 0);
+    punct.x = centru.x + raza * Math.sin(cantitate * 2 * Math.PI / cantitate_maxima);
+    punct.y = centru.y - raza * Math.cos(cantitate * 2 * Math.PI / cantitate_maxima);
+    return punct;
+}
+
+function seteazaLimba(limba, pozitie) {
+    limba.setAttribute("x2", pozitie.x);
+    limba.setAttribute("y2", pozitie.y);
+}
+
+function seteazaCeas(centru, orar, minutar, secundar) {
+    var oraCurenta = new OraCuZecimale(new Date());
+    var poz_sec = pozitieLimba(centru, 130, oraCurenta.secunde, 60);
+    var poz_min = pozitieLimba(centru, 120, oraCurenta.minute, 60);
+    var poz_ore = pozitieLimba(centru, 90, oraCurenta.ore, 12);
+    seteazaLimba(secundar, poz_sec);   
+    seteazaLimba(minutar, poz_min);
+    seteazaLimba(orar, poz_ore);
 }
